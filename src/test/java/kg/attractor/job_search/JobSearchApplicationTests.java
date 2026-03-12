df71af8b5b913689ae20023a083872a242425230
Leyla P. <liya1998@list.ru>
2026-03-12 17:41:03 +0600

    Initialized the project

---
+++ src/test/java/kg/attractor/job_search/JobSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.attractor.job_search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
