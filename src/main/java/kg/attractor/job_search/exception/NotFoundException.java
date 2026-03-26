fc312e597d9edab01eee06c72b9d0a1a8ea9b2a0
Leyla P. <liya1998@list.ru>
2026-03-26 18:20:27 +0600

    Created exception classes

---
+++ src/main/java/kg/attractor/job_search/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package kg.attractor.job_search.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
