596b120c29d3f55b55fb110caf8aa355bf0ac469
Leyla P. <liya1998@list.ru>
2026-03-26 18:09:39 +0600

    Added validation annotations to RespondToVacancyDto

--- src/main/java/kg/attractor/job_search/dto/RespondToVacancyDto.java
+++ src/main/java/kg/attractor/job_search/dto/RespondToVacancyDto.java
@@ -1,5 +1,7 @@
 package kg.attractor.job_search.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +10,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RespondToVacancyDto {
+
+    @NotNull(message = "Resume id cannot be null")
+    @Positive(message = "Resume id must be positive")
     private Integer resumeId;
+
+    @NotNull(message = "Vacancy id cannot be null")
+    @Positive(message = "Vacancy id must be positive")
     private Integer vacancyId;
 }
