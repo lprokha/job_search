d799e0c84c3397c857ad761e2e74c81fa36a8684
Leyla P. <liya1998@list.ru>
2026-03-30 17:20:45 +0600

    Removed user ids from resume and vacancy dto, added builders to dtos

--- src/main/java/kg/attractor/job_search/dto/UpdateUserDto.java
+++ src/main/java/kg/attractor/job_search/dto/UpdateUserDto.java
@@ -2,10 +2,12 @@ package kg.attractor.job_search.dto;
 
 import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class UpdateUserDto {
