bc8468a1d9e2e1191fc9505bf4d589967414fbff
Leyla P. <liya1998@list.ru>
2026-04-09 07:09:52 +0600

    Added GetMapping to AuthController

--- src/main/java/kg/attractor/job_search/controller/AuthController.java
+++ src/main/java/kg/attractor/job_search/controller/AuthController.java
@@ -1,4 +1,18 @@
 package kg.attractor.job_search.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class AuthController {
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String registerPage() {
+        return "register";
+    }
 }
