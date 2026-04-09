aa50959ef8992419b3468d78b2a733b48beaef37
Leyla P. <liya1998@list.ru>
2026-04-09 08:50:36 +0600

    Fixed login page link color and error message

--- src/main/java/kg/attractor/job_search/controller/AuthController.java
+++ src/main/java/kg/attractor/job_search/controller/AuthController.java
@@ -2,14 +2,19 @@ package kg.attractor.job_search.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
 public class AuthController {
 
     @GetMapping("/login")
-    public String loginPage() {
+    public String loginPage(@RequestParam(required = false) String error, Model model) {
+        if (error != null) {
+            model.addAttribute("loginError", true);
+        }
         return "login";
     }
 
