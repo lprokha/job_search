32e4996d7bf6c73dd144d5d38422a5b9829f5ea1
Leyla P. <liya1998@list.ru>
2026-04-13 18:18:36 +0600

    Added CategoryService implementation

---
+++ src/main/java/kg/attractor/job_search/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,21 @@
+package kg.attractor.job_search.service.impl;
+
+import kg.attractor.job_search.dao.CategoryDao;
+import kg.attractor.job_search.model.Category;
+import kg.attractor.job_search.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryServiceImpl implements CategoryService {
+
+    private final CategoryDao categoryDao;
+
+    @Override
+    public List<Category> getAll() {
+        return categoryDao.findAll();
+    }
+}
