514f829e29f339f691f7bde2820079b0ba0796e3
Leyla P. <liya1998@list.ru>
2026-04-16 17:48:49 +0600

    Modified VacancyRepository with JPA query methods

--- src/main/java/kg/attractor/job_search/repository/VacancyRepository.java
+++ src/main/java/kg/attractor/job_search/repository/VacancyRepository.java
@@ -2,6 +2,23 @@ package kg.attractor.job_search.repository;
 
 import kg.attractor.job_search.model.Vacancy;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface VacancyRepository extends JpaRepository<Vacancy, Integer> {
+
+    List<Vacancy> findByIsActiveTrue();
+
+    List<Vacancy> findByIsActiveTrueAndCategoryId(Integer categoryId);
+
+    List<Vacancy> findByAuthorId(Integer authorId);
+
+    @Query("""
+            select v
+            from Vacancy v
+            join RespondedApplicant ra on ra.vacancy.id = v.id
+            where ra.resume.applicant.id = :applicantId
+            """)
+    List<Vacancy> findRespondedByApplicantId(Integer applicantId);
 }
