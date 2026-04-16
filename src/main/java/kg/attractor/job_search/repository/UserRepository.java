3cc91f133dfdf5fa56b923eada812be70c3df389
Leyla P. <liya1998@list.ru>
2026-04-16 17:54:32 +0600

    Modified UserRepository with JPA query methods

--- src/main/java/kg/attractor/job_search/repository/UserRepository.java
+++ src/main/java/kg/attractor/job_search/repository/UserRepository.java
@@ -2,8 +2,17 @@ package kg.attractor.job_search.repository;
 
 import kg.attractor.job_search.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.List;
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    List<User> findByNameContainingIgnoreCase(String name);
+
+    Optional<User> findByPhoneNumber(String phoneNumber);
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
