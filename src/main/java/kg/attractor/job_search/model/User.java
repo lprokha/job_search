2a6c84bb456f89e8ad8328511c22a8cd143f732a
Leyla P. <liya1998@list.ru>
2026-04-16 16:39:57 +0600

    Modified model classes for Hibernate entities

--- src/main/java/kg/attractor/job_search/model/User.java
+++ src/main/java/kg/attractor/job_search/model/User.java
@@ -1,22 +1,29 @@
 package kg.attractor.job_search.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 
-@Data
+@Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private String name;
     private String surname;
     private Integer age;
     private String email;
     private String password;
+
+    @Column(name = "phone_number")
     private String phoneNumber;
+
     private String avatar;
     private AccountType accountType;
     private Boolean enabled;
--- src/main/java/kg/attractor/job_search/model/Vacancy.java
+++ src/main/java/kg/attractor/job_search/model/Vacancy.java
@@ -1,26 +1,59 @@
 package kg.attractor.job_search.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
-@Data
+@Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "vacancies")
 public class Vacancy {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private String name;
     private String description;
-    private Integer categoryId;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
     private Double salary;
+
+    @Column(name = "exp_from")
     private Integer expFrom;
+
+    @Column(name = "exp_to")
     private Integer expTo;
+
+    @Column(name = "is_active")
     private Boolean isActive;
-    private Integer authorId;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private User author;
+
+    @Column(name = "created_date")
     private LocalDateTime createdDate;
+
+    @Column(name = "update_time")
     private LocalDateTime updateTime;
 }
