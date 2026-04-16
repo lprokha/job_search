2f4a2f83a79a556052b80ed07a6cba49929cd6ed
Leyla P. <liya1998@list.ru>
2026-04-16 17:06:31 +0600

    Added security entities with composite keys for JPA

---
+++ src/main/java/kg/attractor/job_search/model/Role.java
@@ -0,0 +1,28 @@
+package kg.attractor.job_search.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "role_name")
+    private String roleName;
+}
