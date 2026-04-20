b504e58eaf498611a3e9dd0aa49523a6aa5f47ee
Leyla P. <liya1998@list.ru>
2026-04-20 18:29:05 +0600

    Modified ResumeRepository for pagination

--- src/main/java/kg/attractor/job_search/repository/ResumeRepository.java
+++ src/main/java/kg/attractor/job_search/repository/ResumeRepository.java
@@ -1,6 +1,8 @@
 package kg.attractor.job_search.repository;
 
 import kg.attractor.job_search.model.Resume;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -8,9 +10,11 @@ import java.util.List;
 
 public interface ResumeRepository extends JpaRepository<Resume, Integer> {
 
-    List<Resume> findByCategory_Id(Integer categoryId);
+    Page<Resume> findByCategory_Id(Integer categoryId, Pageable pageable);
 
-    List<Resume> findByApplicant_Id(Integer applicantId);
+    Page<Resume> findByApplicant_Id(Integer applicantId, Pageable pageable);
+
+    Page<Resume> findAll(Pageable pageable);
 
     @Query("""
             select r
