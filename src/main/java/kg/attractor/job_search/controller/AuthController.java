86ed5f6b3a7b56ca860b1dd2a731fdb3adea5719
Leyla P. <liya1998@list.ru>
2026-04-27 04:54:02 +0600

    Added get and post methods for /forgot-password and /reset-password mapping

--- src/main/java/kg/attractor/job_search/controller/AuthController.java
+++ src/main/java/kg/attractor/job_search/controller/AuthController.java
@@ -1,10 +1,14 @@
 package kg.attractor.job_search.controller;
 
+import jakarta.mail.MessagingException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import kg.attractor.job_search.dto.CreateUserDto;
+import kg.attractor.job_search.dto.ResetPasswordDto;
+import kg.attractor.job_search.exception.NotFoundException;
 import kg.attractor.job_search.model.AccountType;
+import kg.attractor.job_search.model.User;
 import kg.attractor.job_search.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -14,6 +18,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.UnsupportedEncodingException;
 
 @Controller
 @RequiredArgsConstructor
@@ -59,5 +65,59 @@ public class AuthController {
         }
 
         return "redirect:/vacancies";
+    }
+
+    @GetMapping("/forgot-password")
+    public String showForgotPwd() {
+        return "forgot-password-form";
+    }
+
+    @PostMapping("/forgot-password")
+    public String processForgotPassword(HttpServletRequest request, Model model) {
+        try{
+            userService.makeResetPwdLink(request);
+            model.addAttribute("message", "На ваш email отправлена ссылка для смены пароля");
+        } catch (NotFoundException | UnsupportedEncodingException e) {
+            model.addAttribute("error", e.getMessage());
+        } catch (MessagingException e) {
+            model.addAttribute("error", "Ошибка отправки письма");
+        }
+        return "forgot-password-form";
+    }
+
+    @GetMapping("/reset-password")
+    public String showResetPasswordForm(@RequestParam String token, Model model) {
+        try {
+            userService.getByResetPasswordToken(token);
+            ResetPasswordDto form = new ResetPasswordDto();
+            form.setToken(token);
+            model.addAttribute("form", form);
+        } catch (NotFoundException e) {
+            model.addAttribute("error", "Invalid token!");
+        }
+
+        return "reset-password-form";
+    }
+
+    @PostMapping("/reset-password")
+    public String processResetPassword(
+            @Valid @ModelAttribute("form") ResetPasswordDto form,
+            BindingResult bindingResult,
+            Model model
+    ) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("token", form.getToken());
+            return "reset-password-form";
+        }
+
+        try {
+            User user = userService.getByResetPasswordToken(form.getToken());
+            userService.updatePassword(user, form.getPassword());
+            model.addAttribute("message", "Пароль успешно изменен");
+        } catch (NotFoundException e) {
+            model.addAttribute("message", "Invalid token");
+        }
+
+        return "partial/message";
     }
 }
